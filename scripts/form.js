d3041e58657894596900694c15e9e14aa9328415
delapaix105 <delapaix2k@gmail.com>
2024-07-24 12:13:24 +0200

    made some changes on js file to

--- scripts/form.js
+++ scripts/form.js
@@ -64,3 +64,14 @@ const products = [
       }
   });
   
+
+
+// Set current year
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Set last modified date
+const lastModified = new Date(document.lastModified);
+const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+const formattedDate = lastModified.toLocaleDateString('en-US', options);
+document.getElementById('lastModified').textContent = `Last Modification: ${formattedDate}`;
